ffe38f974b936a8c4fd809afde8894029b586e88
James Perry <PjPerry28@gmail.com>
2024-05-17 23:34:43 -0700

    create new nav buttons to scroll to coresponding sections on page/ back to top button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar/NavBar";
 import PhotoCarousel from "./components/PhotoCarousel";
 import AboutMe from "./components/AboutMe";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const App: React.FC = () => {
   return (
@@ -17,9 +18,29 @@ const App: React.FC = () => {
       }}
     >
       <NavBar />
-      <Box sx={{ flexGrow: 1, mt: "64px" }}>
+      <Box id="home" sx={{ flexGrow: 1, mt: "64px" }}>
         <PhotoCarousel />
+      </Box>
+      <Box id="about" sx={{ flexGrow: 1, textAlign: "center", color: "black" }}>
         <AboutMe />
+      </Box>
+      <Box
+        id="gallery"
+        sx={{ flexGrow: 1, textAlign: "center", color: "black" }}
+      >
+        <Typography variant="h2">Gallery Section</Typography>
+        <Typography variant="body1">Photos of food and events...</Typography>
+      </Box>
+      <Box
+        id="contact"
+        sx={{ flexGrow: 1, textAlign: "center", color: "black" }}
+      >
+        <Typography variant="h2">Contact Section</Typography>
+        <Typography variant="body1">Contact information...</Typography>
+      </Box>
+      <Box id="blog" sx={{ flexGrow: 1, textAlign: "center", color: "black" }}>
+        <Typography variant="h2">Blog Section</Typography>
+        <Typography variant="body1">Latest blog posts...</Typography>
       </Box>
     </div>
   );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -6,113 +6,157 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import grey from "@mui/material/colors/grey";
+import Fab from "@mui/material/Fab";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import Gabagool from "./Gabagool";
 
 const color = grey[50];
-const leftPages = ["Home", "Products"];
-const rightPages = ["Pricing", "Blog"];
+const pages = [
+  { label: "About", href: "#about" },
+  { label: "Gallery", href: "#gallery" },
+  { label: "Contact", href: "#contact" },
+  { label: "Blog", href: "#blog" },
+];
 
 function NavBar() {
+  const [showBackToTop, setShowBackToTop] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowBackToTop(true);
+      } else {
+        setShowBackToTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <AppBar
-      position="fixed"
-      sx={{
-        backgroundColor: color,
-        top: 0,
-        marginBottom: 0,
-        paddingBottom: 0,
-        height: "64px",
-      }}
-    >
-      <Container maxWidth="xl" disableGutters>
-        <Toolbar disableGutters>
-          {/* Left-side navigation items */}
-          <Box sx={{ display: "flex", flexGrow: 0.5, justifyContent: "right" }}>
-            {leftPages.map((page) => (
-              <Button
-                key={page}
+    <>
+      <AppBar
+        position="fixed"
+        sx={{
+          backgroundColor: color,
+          borderTop: { xs: "4px solid black", md: "none" }, // Add black line on top for mobile
+        }}
+      >
+        <Container maxWidth="xl" disableGutters>
+          <Toolbar disableGutters>
+            {/* Left-side navigation items */}
+            <Box
+              sx={{ display: "flex", flexGrow: 0.5, justifyContent: "right" }}
+            >
+              {pages.slice(0, 2).map((page) => (
+                <Button
+                  key={page.label}
+                  href={page.href}
+                  sx={{
+                    my: 2,
+                    color: "black",
+                    display: "block",
+                    borderBottom: "2px solid transparent",
+                    fontSize: { xs: "1rem", md: "1.25rem" }, // Adjust font size
+                    mx: { xs: 1, md: 2 }, // Adjust margin for spacing
+                    "&:hover": {
+                      borderBottom: "2px solid black",
+                      background: "none",
+                    },
+                    "&:focus": {
+                      outline: "none",
+                      borderBottom: "2px solid black",
+                    },
+                  }}
+                >
+                  {page.label}
+                </Button>
+              ))}
+            </Box>
+
+            {/* Center text with logo in between */}
+            <Box sx={{ display: "flex", alignItems: "center", mx: 1 }}>
+              <Typography
+                variant="h6"
+                noWrap
+                component="span"
                 sx={{
-                  my: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  fontSize: { xs: "1rem", md: "1.5rem" }, // Adjust font size
                   color: "black",
-                  display: "block",
-                  borderBottom: "2px solid transparent",
-                  "&:hover": {
-                    borderBottom: "2px solid black",
-                    background: "none",
-                  },
-                  "&:focus": {
-                    outline: "none",
-                    borderBottom: "2px solid black",
-                  },
+                  textDecoration: "none",
                 }}
               >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          {/* Center text with logo in between */}
-          <Box sx={{ display: "flex", alignItems: "center", mx: 1 }}>
-            <Typography
-              variant="h6"
-              noWrap
-              component="span"
-              sx={{
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "black",
-                textDecoration: "none",
-              }}
-            >
-              PNW
-            </Typography>
-            <Gabagool /> {/* Use the Logo component */}
-            <Typography
-              variant="h6"
-              noWrap
-              component="span"
-              sx={{
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "black",
-                textDecoration: "none",
-              }}
-            >
-              CHEF
-            </Typography>
-          </Box>
-
-          {/* Right-side navigation items */}
-          <Box sx={{ display: "flex", flexGrow: 0.5 }}>
-            {rightPages.map((page) => (
-              <Button
-                key={page}
+                PNW
+              </Typography>
+              <Gabagool /> {/* Use the Logo component */}
+              <Typography
+                variant="h6"
+                noWrap
+                component="span"
                 sx={{
-                  my: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  fontSize: { xs: "1rem", md: "1.5rem" }, // Adjust font size
                   color: "black",
-                  display: "block",
-                  borderBottom: "2px solid transparent",
-                  "&:hover": {
-                    borderBottom: "2px solid black",
-                    background: "none",
-                  },
-                  "&:focus": {
-                    outline: "none",
-                    borderBottom: "2px solid black",
-                  },
+                  textDecoration: "none",
                 }}
               >
-                {page}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+                CHEF
+              </Typography>
+            </Box>
+
+            {/* Right-side navigation items */}
+            <Box sx={{ display: "flex", flexGrow: 0.5 }}>
+              {pages.slice(2).map((page) => (
+                <Button
+                  key={page.label}
+                  href={page.href}
+                  sx={{
+                    my: 2,
+                    color: "black",
+                    display: "block",
+                    borderBottom: "2px solid transparent",
+                    fontSize: { xs: "1rem", md: "1.25rem" }, // Adjust font size
+                    mx: { xs: 1, md: 2 }, // Adjust margin for spacing
+                    "&:hover": {
+                      borderBottom: "2px solid black",
+                      background: "none",
+                    },
+                    "&:focus": {
+                      outline: "none",
+                      borderBottom: "2px solid black",
+                    },
+                  }}
+                >
+                  {page.label}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {showBackToTop && (
+        <Fab
+          color="secondary"
+          size="small"
+          onClick={handleBackToTop}
+          sx={{ position: "fixed", bottom: 16, right: 16 }}
+        >
+          <KeyboardArrowUpIcon />
+        </Fab>
+      )}
+    </>
   );
 }
 
